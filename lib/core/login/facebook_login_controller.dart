213db97ca19cdc08778b52022399eb4a2cf4d441
Carlos <echoulen@gmail.com>
2019-01-31 18:45:35 +0800

    package: install flutter_facebook_login

---
+++ lib/core/login/facebook_login_controller.dart
@@ -0,0 +1,14 @@
+import 'package:mafia_app/core/login/login_controller.dart';
+
+class FaceBookLoginController implements LoginController {
+
+  @override
+  Future login() async {
+    // TODO
+  }
+
+  @override
+  Future logout() async {
+    // TODO
+  }
+}
