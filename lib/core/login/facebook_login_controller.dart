3e57543177a2c1e190d7bbba620cdfd8a44c527e
Carlos <echoulen@gmail.com>
2019-02-12 10:05:31 +0800

    init flutter_facebook_login

--- lib/core/login/facebook_login_controller.dart
+++ lib/core/login/facebook_login_controller.dart
@@ -1,14 +1,29 @@
 import 'package:mafia_app/core/login/login_controller.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
-class FaceBookLoginController implements LoginController {
+class FacebookLoginController implements LoginController {
+  final _facebookLogin = FacebookLogin();
 
   @override
   Future login() async {
-    // TODO
+    final result = await _facebookLogin.logInWithReadPermissions(['email']);
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        print("success");
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print("cancelledByUser");
+        break;
+      case FacebookLoginStatus.error:
+        print("error");
+        break;
+    }
+
+    return result;
   }
 
   @override
-  Future logout() async {
-    // TODO
+  Future logout() {
+    return _facebookLogin.logOut();
   }
 }
